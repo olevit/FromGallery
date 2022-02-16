132f5579c43be25bf395b4d5d125940e76a5b112
admin <olevist@gmail.com>
2022-02-16 19:54:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fromgallery/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.fromgallery;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String KEY_User_Document1 = "doc1";
+    ImageView imageView;
+    Button button;
+
+    private String Document_img1="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+    private void selectImage() {
+        final CharSequence[] options = {"Choose from Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+            }
+        });
+        builder.show();
+    }
+
+    public void onClick(View view) {
+        if (Document_img1.equals("") || Document_img1.equals(null)) {
+            ContextThemeWrapper ctw = new ContextThemeWrapper( MainActivity.this, R.style.Theme_AppCompat_Dialog);
+            final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ctw);
+            alertDialogBuilder.setTitle("Id Prof Can't Empty ");
+            alertDialogBuilder.setMessage("Id Prof Can't empty please select any one document");
+            alertDialogBuilder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+
+                }
+            });
+            alertDialogBuilder.show();
+            return;
+        }
+    }
+}
